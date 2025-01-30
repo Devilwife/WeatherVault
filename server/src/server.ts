c67fb5a2f1aca735cbfd38e18e91c3c1973b45cc
Devilwife <nicole.barger22@gmail.com>
2025-01-29 23:44:33 -0500

    updated historyservice

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,8 @@
+// TODO: Serve static files of entire client dist folder
+
+// TODO: Implement middleware for parsing JSON and urlencoded form data
+
+// TODO: Implement middleware to connect the routes
 import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
@@ -9,12 +14,32 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
+// Serve static files from the 'dist' folder
+app.use(express.static('../client/dist'));
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// TODO: Implement middleware to connect the routes
+// Connect the routes
 app.use(routes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+// import dotenv from 'dotenv';
+// import express from 'express';
+// dotenv.config();
+
+// // Import the routes
+// import routes from './routes/index.js';
+
+// const app = express();
+
+// const PORT = process.env.PORT || 3001;
+
+
+// app.use(routes);
+
+// // Start the server on the port
+// app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
